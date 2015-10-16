03f8bf91468a2023946371ad987e9dfaffcc7850
Chui <lordbabypanda@gmail.com>
2015-10-16 11:21:01 -0500

    Doing my own Helicopter from bottom!

---
+++ Assets/Scripts/testInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class testInput : MonoBehaviour
+{
+    /*
+    WASD's Q, E, Z, Shift for input
+
+    */
+
+    void InputCookies()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+
+        }
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+
+        }
+    }
+}
